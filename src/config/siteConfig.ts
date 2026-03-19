e6c8f5b082963485b4915403e597bae6a614d251
bit7428 <3904021971@qq.com>
2026-03-20 00:18:46 +0800

    修复格式

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -34,7 +34,7 @@ export const siteConfig: SiteConfig = {
 		// 主题色的默认色相，范围从 0 到 360。例如：红色：0，青色：200，蓝绿色：250，粉色：345
 		hue: 360,
 		// 是否对访问者隐藏主题色选择器
-		fixed: true,
+		fixed: false,
 		// 默认模式："light" 亮色，"dark" 暗色，"system" 跟随系统
 		defaultMode: "dark",
 	},
@@ -84,7 +84,7 @@ export const siteConfig: SiteConfig = {
 		// 导航菜单对齐方式，left：左对齐，center：居中
 		menuAlign: "center",
 		// 导航栏图标和标题是否跟随主题色
-		followTheme: false,
+		followTheme: true,
 		// 导航栏是否固定在顶部并始终可见
 		stickyNavbar: true,
 	},
